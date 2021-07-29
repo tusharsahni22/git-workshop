c5236e797b819f786f5324e53a35ac41eda48ae0
Tushar Sahni <tusharsahni22@gmail.com>
2021-07-29 22:16:41 +0530

    solving conflict by using both changes

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 console.log("Tushar Sahni")
+<<<<<<< HEAD
 console.log("branch name = development")
 // edited by dev 1
+=======
+console.log("branch name= new feature")
+// edited by dev 2
+>>>>>>> new-features
